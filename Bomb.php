e4a99f095aaac630d72cbad65ed29c8c06ec594e
DEVIL8240 <111481085+DEVIL8240@users.noreply.github.com>
2023-09-06 23:44:03 +0530

    Bomb.php

---
+++ Bomb.php
@@ -0,0 +1,411 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Fastest : 2023 Unlimited Call Bomber | Call Bomber Online apk.</title>
+<meta name="language" content="English">
+<meta name="theme-color" content="#000">
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<meta http-equiv="content-language" content="en">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+
+<meta name="title" content="Fastest : 2023 Unlimited Call Bomber | Call Bomber Online apk.">
+<meta name="description" content="Hang Phone , Prank with friends : Send Upto 500+ Calls in One Click! Enter the Number, Press Submit, and Start callbomber online!.">
+<meta name="keywords" content="call bomber,call bomber in,call bomber online,call boomer,call bomber apk,fake call online,sms call bomber,fake call bomber apk,unlimited call bomber,otp bomber,miss call bomber,call bomber com,unlimited call bomber apk,call bomber unlimited,miss call bomber online,call boom,call bomber free,bumper call,call bomber apk download,call bomber app,prank call bomber,call bom,call bomber india,spam call bomber,whatsapp call bomber,online pro xyz,best call bomber,boomcall,call bomber app download,call bomber prank,turbo call bomber,call bomber download,call bomber new,sms call bomber apk,call bomber website,phone call bomber,bombing call,boombit call,call blast prank,unlimited call bomber online,turbo bomber call,best call bomber app,call blaster online,call bomb a phone number,call bomber biz,call bomber site,call bombing prank,call bomber link,caller bomb,call and sms bomber apk,call bomber online free, Fake Call Bomber Apk">
+<meta name="author" content="DEVIL">
+
+<meta itemprop="name" content="call bomber,call bomber in,call bomber online,call boomer,call bomber apk,fake call online,sms call bomber,fake call bomber apk,unlimited call bomber,otp bomber,miss call bomber,call bomber com,unlimited call bomber apk,call bomber unlimited,miss call bomber online,call boom,call bomber free,bumper call,call bomber apk download,call bomber app,prank call bomber,call bom,call bomber india,spam call bomber,whatsapp call bomber,online pro xyz,best call bomber,boomcall,call bomber app download,call bomber prank,turbo call bomber,call bomber download,call bomber new,sms call bomber apk,call bomber website,phone call bomber,bombing call,boombit call,call blast prank,unlimited call bomber online,turbo bomber call,best call bomber app,call blaster online,call bomb a phone number,call bomber biz,call bomber site,call bombing prank,call bomber link,caller bomb,call and sms bomber apk,call bomber online free ,Fake Call Bomber Apk">
+<meta itemprop="description" content="Hang Phone , Prank with friends : Send Upto 500+ Calls in One Click! Enter the Number, Press Submit, and Start callbomber online!.">
+
+<meta http-equiv="cache-control" content="max-age=0">
+<meta http-equiv="cache-control" content="no-cache">
+<meta http-equiv="expires" content="0">
+<meta http-equiv="pragma" content="no-cache">
+
+<meta property="og:image" alt="Callbomber" content="include/images/icon.webp">
+<meta property="og:title" content="Fastest : 2023 Unlimited Call Bomber | Call Bomber Online apk.">
+<meta property="og:type" content="website">
+<meta property="og:description" content="Hang Phone , Prank with friends : Send Upto 500+ Calls in One Click! Enter the Number, Press Submit, and Start callbomber online!.">
+<link rel="canonical" href="https://www.softdownload.in/Call-Bomber" />
+<link rel="icon" alt="Callbomber" type="image/png" href="include/images/icon.png">
+<link href="https://kit-pro.fontawesome.com/releases/v5.15.1/css/pro.min.css" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Shrikhand&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,wght@0,400;1,300&family=Sedgwick+Ave+Display&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Merienda+One&display=swap" rel="stylesheet">
+<script src="https://www.google.com/recaptcha/api.js" type="ee2842fd1e7594bc15cc69f6-text/javascript"></script>
+<link rel="stylesheet" href="../include/css/style.css">
+<link rel="stylesheet" href="../include/css/service.css">
+</head>
+<style type="text/css">
+     #load {
+        display: none;
+    position: fixed;
+    width: 100%;
+    height: 110vh;
+    background: #000;
+    z-index: 999999;    
+}
+#load img{
+    width: 30rem;
+    position: relative;
+    top: 5.5rem;
+}
+#load .lh{
+    font-family: Shrikhand;
+    color: #5d6889;
+    font-size: 4rem;
+    font-weight: bold;
+    text-align: center;
+    padding-top: 1.3rem;
+    word-spacing: 1rem;
+}
+#load .site{
+    text-align: center;
+    color: #a6a6a6;
+    text-align: center;
+    font-weight: bold;
+    letter-spacing: 0.1rem;
+    word-spacing: 0.2rem;
+    font-size: 2.5rem;
+    font-family: Merienda One;
+}
+#load span{
+    font-size: 1.2rem;
+    color: #a6a6a6;
+}
+#load button{
+    padding: 0.7rem 0.5rem;
+    font-weight: bolder;
+    background-color: #03C0FF;
+    font-family: Merienda One;
+    border-radius: 0.5rem;
+    cursor: pointer;
+}
+@media screen and (max-width: 769px){
+    #load .lh{
+        position: relative;
+        top: -6.7rem;
+    }
+    #load img{
+        width: 30rem;
+        position: relative;
+        top: 1.3rem;
+    }
+    #load .site{
+        position: relative;
+        top: -9rem;
+        letter-spacing: 0.1rem;
+        word-spacing: 0.1rem;
+        padding-top: 0%;
+        font-size: 2rem;
+    }
+    #load span{
+        font-size: 1.2rem;
+    }
+    #load button{
+        padding: 1rem 2rem;
+        font-weight: bolder;
+        background-color: #03C0FF;
+        font-family: Merienda One;
+        border-radius: 0.5rem;
+        cursor: pointer;
+        position: relative;
+        top: -7.5rem;
+        outline: none;
+        border: none;
+    }
+}
+
+    </style>
+<body class="preloader">
+<div id="load">
+<div style="text-align: center;"><img alt="ad blocker" src="include/images/ad.jpg"></div>
+<p class="lh">AD-BLOCKER DETECTED</p>
+<p class="site">We Have Detected That You Are Using An AD-BLOCKING Plugin In Your Browser.</p>
+<p class="site">The Revenue We Earn By This Advertisement Is Used To Manage This Website.So, We Request To You To Wishlist Our Website In Your Ad-Blocking PlugIn. </p>
+<a href><div style="text-align: center;"><button>TRY - AGAIN</button></div></a>
+</div>
+<header>
+<i class="fas fa-signal-alt-3" id="nav_icon"></i>
+<h1><a href>Call-Bomber</a></h1></center>
+<a href><i class="fas fa-bell" id="nav_notify"></i></a>
+<a href="https://vip.softdownload.in"><i class="fad fa-user-cowboy" id="nav_profile_icon"></i></a>
+</header>
+<nav class="nav">
+<div class="nav__links">
+<div class="nav_background"><br>
+<a href="Admin-Profile">
+<div class="nav__link">
+<img src="../include/images/NaYaN.webp">
+<span class="admin_name">DEVIL</span><br>
+<span class="admin_tag">[ ADMIN ]</span>
+</div>
+</a><br>
+<a href="Home">
+<div class="nav__link">
+<button>
+<i class="fas fa-home"></i>
+<span class="home_tag">HOME</span>
+</button>
+</div>
+</a><br>
+
+<div class="nav__link" onclick="if (!window.__cfRLUnblockHandlers) return false; dropDown()" data-cf-modified-ee2842fd1e7594bc15cc69f6->
+<button type="button">
+<i class="fas fa-tools"></i>
+<span class="tool_tag">TOOL's</span>
+<i class="fas fa-chevron-down" id="drop_down"></i>
+</button>
+</div><br>
+
+<div id="dropdown_menu">
+<a href="Number-Details">
+<div id="drop_icon" class="child_3">
+<button class="drop_btn">
+<i class="far fa-user-visor"></i>
+<span>NUMBER DETAILS</span>
+</button>
+</div>
+</a><br>
+<a href="International-bomber">
+<div id="drop_icon" class="child_3">
+<button class="drop_btn">
+<i class="fas fa-phone-alt"></i>
+<span>International bomber</span>
+</button>
+</div>
+</a><br>
+<a href="SmS-BombeR">
+<div id="drop_icon" class="child_3">
+<button class="drop_btn">
+<i class="fas fa-bomb"></i>
+<span>SMS BOMBER</span>
+</button>
+</div><br>
+</a>
+<a href="WhatsApp-BombeR">
+<div id="drop_icon" class="child_5">
+<button class="drop_btn">
+<i class="fas fa-bomb"></i>
+<span style>WHATSAPP BOMBER</span>
+</button>
+</div>
+</a><br>
+<a href="Call-Bomber">
+<div id="drop_icon" class="child_4">
+<button class="drop_btn">
+<i class="fas fa-bomb"></i>
+<span>CALL BOMBER</span>
+</button>
+</div>
+</a><br>
+<a href="Number-Protector">
+<div id="drop_icon" class="child_6">
+<button class="drop_btn">
+<i class="far fa-shield-alt"></i>
+<span>NUMBER PROTECTOR</span>
+</button>
+</div>
+</a><br>
+<a href="Custom-Sms">
+<div id="drop_icon" class="child_9">
+<button class="drop_btn">
+<i class="fas fa-comment-alt-lines"></i>
+<span style>CUSTOM MASSAGE</span>
+</button>
+</div>
+</a><br>
+<a href="INFO-SERVICE">
+<div id="drop_icon" class="child_6">
+<button class="drop_btn">
+<i class="about_tag"></i>
+<span>INFO SERVICE</span>
+</button>
+</div>
+</a><br>
+</div>
+
+<a href="Follow-Us">
+<div class="nav__link">
+<button>
+<i class="fas fa-plus-circle"></i>
+<span class="follow_tag">FOLLOW</span>
+</button>
+</div>
+</a><br>
+<a href="About-Us">
+<div class="nav__link">
+<button>
+<i class="fas fa-address-card"></i>
+<span class="about_tag">ABOUT</span>
+</button>
+</div>
+</a><br>
+<a href="Contact-Us">
+<div class="nav__link">
+<button>
+<i class="fas fa-envelope"></i>
+<span class="contact_tag">CONTACT</span>
+</button>
+</div>
+</a><br>
+<a href="Privacy-Policy ">
+<div class="nav__link">
+<button>
+<i class="fas fa-comment-alt-edit"></i>
+<span class="policy_tag">POLICY</span>
+</button>
+</div>
+</a><br>
+<a href="Share-Us">
+<div class="nav__link">
+<button>
+<i class="fas fa-share-alt"></i>
+<span class="shear_tag">SHARE</span>
+</button>
+</div>
+</a><br>
+</div>
+</div>
+<div class="nav__overlay"></div>
+</nav>
+<main><br><br><br><br><br><br>
+<center>
+<div class="true-caller">
+<p>Best unlimited Free sms call bomber service online 2023.</p>
+<p><span>What Is Call Bomber Online?</span></p>
+<p> Call Bomber Online is a fake callbomber online app, Which can send 500+ fake call bomber online in one Click..</p>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4189215097730023" crossorigin="anonymous" type="ee2842fd1e7594bc15cc69f6-text/javascript"></script>
+
+<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-4189215097730023" data-ad-slot="1790345774" data-ad-format="auto" data-full-width-responsive="true"></ins>
+<script type="ee2842fd1e7594bc15cc69f6-text/javascript">
+             (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
+<p><span>How To Use Call Bomber online?</span></p>
+<p>Step 1: Enter the number you want to bombitup in form given below.</p>
+<p>Step 2: Now hit the Bomb button to activate fake call online.</p>
+<p>SUPER FAST CALL BOMBER<span class="blink_text"><a href="https://vip.softdownload.in/REGISTER"> UPGRADE TO VIP CLICK HEAR</a></span></p>
+<p> Free Access - Key Join Our Channel</p>
+<span id="t"><a href="https://telegram.me/devil_box">[ Click Here For New Key ]</a></span><br><br>
+<form name="true-caller" onsubmit="if (!window.__cfRLUnblockHandlers) return false; return formValidation_true_caller()" method="POST" action data-cf-modified-ee2842fd1e7594bc15cc69f6->
+<div class="input_box" id="number">
+<button class="input_icon"><i class="fas fa-phone-alt"></i></button>
+<input type="number" name="number" placeholder="Enter Your Number" onkeyup="if (!window.__cfRLUnblockHandlers) return false; getNumber()" autocomplete="off" data-cf-modified-ee2842fd1e7594bc15cc69f6->
+</div><span class="number_msg"></span><br>
+<div class="input_box" id="key">
+<button class="input_icon"><i class="fas fa-key"></i></button>
+<input type="text" name="key" placeholder="Enter Your Access Key" onkeyup="if (!window.__cfRLUnblockHandlers) return false; getKey()" autocomplete="off" data-cf-modified-ee2842fd1e7594bc15cc69f6->
+</div><span class="key_msg"></span><br>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4189215097730023" crossorigin="anonymous" type="ee2842fd1e7594bc15cc69f6-text/javascript"></script>
+
+<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-4189215097730023" data-ad-slot="1790345774" data-ad-format="auto" data-full-width-responsive="true"></ins>
+<script type="ee2842fd1e7594bc15cc69f6-text/javascript">
+                                 (adsbygoogle = window.adsbygoogle || []).push({});
+                            </script>
+<div class="g-recaptcha" data-theme="dark" data-sitekey="6LfRpXQeAAAAALoQTnp2upb0k0LaItxv0V-i_E32"></div><br>
+<span></span>
+<div class="hide"><br></div>
+<button type="submit" name="submit">BOMB</button>
+</form>
+<br>
+<span></span><br><br>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4189215097730023" crossorigin="anonymous" type="ee2842fd1e7594bc15cc69f6-text/javascript"></script>
+<a href="#" class="telegramim_button telegramim_shadow telegramim_pulse" id="tel"><b> DEVELOPED BY - DEVIL </b></a>
+<br><br><hr>
+</div><br><br>
+</center>
+</main>
+<script src="../include/js/include.js" type="ee2842fd1e7594bc15cc69f6-text/javascript"></script>
+<script src="../../include/js/form-val.js" type="ee2842fd1e7594bc15cc69f6-text/javascript"></script>
+<script src="../../include/js/telegram.js" type="ee2842fd1e7594bc15cc69f6-text/javascript"></script>
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-V2MEYPLY9Z" type="ee2842fd1e7594bc15cc69f6-text/javascript"></script>
+<script type="ee2842fd1e7594bc15cc69f6-text/javascript">
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-V2MEYPLY9Z');
+</script>
+<script type="ee2842fd1e7594bc15cc69f6-text/javascript">
+        var preloader = document.getElementById('load');
+preloader.style.display = "none";
+function adBlock(){
+    setTimeout(function() {
+        preloader.style.display = "block";
+    },1);
+}
+    </script>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4189215097730023" crossorigin="anonymous" onerror="adBlock()" type="ee2842fd1e7594bc15cc69f6-text/javascript"></script>
+<script type="application/ld+json">
+{
+  "@context": "https://schema.org",
+  "@type": "FAQPage",
+  "mainEntity": [{
+    "@type": "Question",
+    "name": "What Is Call Bomber Online?",
+    "acceptedAnswer": {
+      "@type": "Answer",
+      "text": " Call Bomber Online is a fake callbomber online app, Which can send 500+ fake call bomber online in one Click.."
+    }
+  },{
+    "@type": "Question",
+    "name": "How To Use Call Bomber online?",
+    "acceptedAnswer": {
+      "@type": "Answer",
+      "text": "Step 1: Enter the number you want to bombitup in form given below.
+Step 2: Now hit the Bomb button to activate fake call online."
+    }
+  }]
+}
+</script>
+<script type="application/ld+json">
+{
+  "@context": "https://schema.org/", 
+  "@type": "Product", 
+  "name": "call bomber online 2023",
+  "image": "https://www.softdownload.in/include/images/icon.webp",
+  "description": " Call Bomber Online is a fake callbomber online app, Which can send 500+ fake call bomber online in one Click..",
+  "brand": {
+    "@type": "Brand",
+    "name": "call bomber"
+  },
+  "aggregateRating": {
+    "@type": "AggregateRating",
+    "ratingValue": "4.3",
+    "ratingCount": "10000"
+  }
+}
+</script>
+<script type="application/ld+json">
+{
+  "@context": "https://schema.org/",
+  "@type": "HowTo",
+  "name": "Call Bomber Online 2023",
+  "description": "Call Bomber Online is a fake call online app, which can send a huge number of WhatsApp calls in one go.",
+  "image": "https://www.softdownload.in/include/images/icon.webp",
+  "totalTime": "PT2M",
+  "estimatedCost": {
+    "@type": "MonetaryAmount",
+    "currency": "INR",
+    "value": "1"
+  },
+  "step": [
+    {
+      "@type": "HowToStep",
+      "text": "Step 1: Enter the number you want to bombitup in the form given below.",
+      "name": "Enter the number",
+      "image": "https://www.softdownload.in/include/images/icon.webp"
+    },
+    {
+      "@type": "HowToStep",
+      "text": "Step 2: Hit the Bomb button to activate the fake call online.",
+      "name": "Activate the fake call",
+      "image": "https://www.softdownload.in/include/images/icon.webp"
+    }
+  ],
+  "url": "https://www.softdownload.in/call-Bomber"
+}
+</script>
+<script src="/cdn-cgi/scripts/7d0fa10a/cloudflare-static/rocket-loader.min.js" data-cf-settings="ee2842fd1e7594bc15cc69f6-|49" defer></script></body>
+</html>
